17712ad37cef3a4b7e93340ffd27ed76ca1abbc7
Clément DOUIN <clement.douin@posteo.net>
2025-10-23 23:18:52 +0200

    ci: fix builds

--- flake.nix
+++ flake.nix
@@ -2,7 +2,9 @@
   description = "CLI to manage OAuth 2.0 access tokens";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/staging-next";
+    };
     fenix = {
       url = "github:nix-community/fenix/monthly";
       inputs.nixpkgs.follows = "nixpkgs";
